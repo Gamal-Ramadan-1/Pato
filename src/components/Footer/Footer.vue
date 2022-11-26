ae5bafe36e143910dee22b83ed2626d9c00646b5
Gamal Ramadan <gamal.ramadan.fe@gmail.com>
2022-11-26 18:45:21 +0200

    Add Latest-Twitter In Footer Section

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -24,6 +24,38 @@
                         </p>
                     </div>
                 </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 my-3">
+                    <div class="lastest-twitter">
+                        <h5 class="text-light text-uppercase mb-4">lastest twitter</h5>
+                        <a href="#">
+                            <span class="text-light d-inline-block">
+                                <i class="fa-brands fa-twitter"></i>
+                            </span>
+                            @colorlib
+                        </a>
+                        <p class="m-0">
+                            Activello is a good option. It has a slider built into that displays the featured image in
+                            the slider.
+                        </p>
+                        <p>
+                            <a href="#">https://buff.ly/2zaSfAQ</a>
+                        </p>
+                        <div class="date my-3">21 Dec 2017</div>
+                        <a href="#">
+                            <span class="text-light d-inline-block">
+                                <i class="fa-brands fa-twitter"></i>
+                            </span>
+                            @colorlib
+                        </a>
+                        <p class="m-0">
+                            Activello is a good option. It has a slider built into that displays
+                            the featured
+                            <a href="#">https://buff.ly/2zaSfAQ</a>
+
+                        </p>
+                        <div class="date mt-3">21 Dec 2017</div>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
@@ -33,8 +65,18 @@ export default {
     name: 'Footer'
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @import '@/Scss/Main.scss';
+
+a {
+    text-decoration: none !important;
+    color: $Main-Color !important;
+}
+
+a,
+p {
+    font-size: 13px;
+}
 
 footer {
     background-color: $Footer-Color;
@@ -42,6 +84,19 @@ footer {
     .contact-us {
         p {
             color: #999;
+        }
+    }
+
+    .lastest-twitter {
+
+
+
+        p {
+            color: #999;
+        }
+
+        .date {
+            color: rgb(79, 78, 78);
             font-size: 13px;
         }
     }
