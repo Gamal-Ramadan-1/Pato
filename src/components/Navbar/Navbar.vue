7887d9876acb1b575aa4413c385baaf9b76b2bd5
Gamal Ramadan <gamal.ramadan.fe@gmail.com>
2022-11-28 23:20:27 +0200

    Fix NavBar

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,97 +1,109 @@
 <template>
     <section class="nav-1" v-show="scY < 250">
         <div class="line p-1 m-0"></div>
-        <nav class="navbar navbar-expand-lg py-3">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="@/Imgs/logo.png.webp">
-                </a>
-                <ul class="navbar-nav">
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/" tag="a" class="nav-link p-0 m-2">Home</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/menu" tag="a" class="nav-link p-0 m-2">menu</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/reservation" tag="a" class="nav-link p-0 m-2">reservation</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/gallary" tag="a" class="nav-link p-0 m-2">gallary</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/about" tag="a" class="nav-link p-0 m-2">about</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/blog" tag="a" class="nav-link p-0 m-2">blog</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/contact" tag="a" class="nav-link p-0 m-2">contact</RouterLink>
-                    </li>
-                </ul>
-                <div class="icons float-start d-flex justify-content-center align-items-center">
-                    <p class="d-inline-block m-0">
-                        <i class="fa-brands fa-youtube mx-2"></i>
-                    </p>
-                    <p class="d-inline-block m-0">
-                        <i class="fa-brands fa-facebook-f mx-2"></i>
-                    </p>
-                    <p class="d-inline-block m-0">
-                        <i class="fa-brands fa-twitter mx-2"></i>
-                    </p>
-                    <p @click="showSlideNav()" class="d-inline-block m-0">
-                        <i class="fa-solid fa-bars mx-2"></i>
-                    </p>
+        <div class="container">
+            <div class="row p-3 justify-content-center align-items-center">
+                <div class="col p-0">
+                    <a href="#">
+                        <img src="@/Imgs/logo.png.webp">
+                    </a>
+                </div>
+                <div class="col d-lg-inline-block d-none p-0">
+                    <ul class="m-0 p-0">
+                        <li class="text-uppercase">
+                            <RouterLink to="/" tag="a" class="p-0 ms-4">Home</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/menu" tag="a" class="p-0 ms-4">menu</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/reservation" tag="a" class="p-0 ms-4">reservation</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/gallary" tag="a" class="p-0 ms-4">gallary</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/about" tag="a" class="p-0 ms-4">about</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/blog" tag="a" class="p-0 ms-4">blog</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/contact" tag="a" class="p-0 ms-4">contact</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col p-0">
+                    <div class="icons float-end">
+                        <p class="d-inline-block m-0">
+                            <i class="fa-brands fa-youtube mx-2"></i>
+                        </p>
+                        <p class="d-inline-block m-0">
+                            <i class="fa-brands fa-facebook-f mx-2"></i>
+                        </p>
+                        <p class="d-inline-block m-0">
+                            <i class="fa-brands fa-twitter mx-2"></i>
+                        </p>
+                        <p @click="showSlideNav()" class="d-inline-block m-0">
+                            <i class="fa-solid fa-bars mx-2"></i>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </nav>
+        </div>
     </section>
     <section class="nav-2" v-show="scY > 340">
         <div class="line p-1 m-0"></div>
-        <nav class="navbar navbar-expand-lg py-3">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="@/Imgs/logo2.png.webp">
-                </a>
-                <ul class="navbar-nav">
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/" tag="a" class="nav-link p-0 m-2">Home</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/menu" tag="a" class="nav-link p-0 m-2">menu</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/reservation" tag="a" class="nav-link p-0 m-2">reservation</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/gallary" tag="a" class="nav-link p-0 m-2">gallary</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/about" tag="a" class="nav-link p-0 m-2">about</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/blog" tag="a" class="nav-link p-0 m-2">blog</RouterLink>
-                    </li>
-                    <li class="nav-item text-uppercase px-4">
-                        <RouterLink to="/contact" tag="a" class="nav-link p-0 m-2">contact</RouterLink>
-                    </li>
-                </ul>
-                <div class="icons float-start d-flex justify-content-center align-items-center">
-                    <p class="d-inline-block m-0">
-                        <i class="fa-brands fa-youtube mx-2"></i>
-                    </p>
-                    <p class="d-inline-block m-0">
-                        <i class="fa-brands fa-facebook-f mx-2"></i>
-                    </p>
-                    <p class="d-inline-block m-0">
-                        <i class="fa-brands fa-twitter mx-2"></i>
-                    </p>
-                    <p @click="showSlideNav()" class="d-inline-block m-0">
-                        <i class="fa-solid fa-bars mx-2"></i>
-                    </p>
+        <div class="container">
+            <div class="row p-3 justify-content-center align-items-center">
+                <div class="col p-0">
+                    <a href="#">
+                        <img src="../../Imgs/logo2.png.webp">
+                    </a>
+                </div>
+                <div class="col d-lg-inline-block d-none p-0">
+                    <ul class="m-0 p-0">
+                        <li class="text-uppercase">
+                            <RouterLink to="/" tag="a" class="p-0 ms-4">Home</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/menu" tag="a" class="p-0 ms-4">menu</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/reservation" tag="a" class="p-0 ms-4">reservation</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/gallary" tag="a" class="p-0 ms-4">gallary</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/about" tag="a" class="p-0 ms-4">about</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/blog" tag="a" class="p-0 ms-4">blog</RouterLink>
+                        </li>
+                        <li class="text-uppercase">
+                            <RouterLink to="/contact" tag="a" class="p-0 ms-4">contact</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col p-0">
+                    <div class="icons float-end">
+                        <p class="d-inline-block m-0">
+                            <i class="fa-brands fa-youtube mx-2"></i>
+                        </p>
+                        <p class="d-inline-block m-0">
+                            <i class="fa-brands fa-facebook-f mx-2"></i>
+                        </p>
+                        <p class="d-inline-block m-0">
+                            <i class="fa-brands fa-twitter mx-2"></i>
+                        </p>
+                        <p @click="showSlideNav()" class="d-inline-block m-0">
+                            <i class="fa-solid fa-bars mx-2"></i>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </nav>
+        </div>
     </section>
     <SideNavmenu class="sideNav m-0" />
     <RouterView />
@@ -140,17 +152,30 @@ $(document).ready(function () {
         width: 100%;
     }
 
-    .nav-link {
-        color: white;
-        font-weight: 200;
-        transition: all .3s ease-in-out;
+    ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0px;
+
+        li {
+            list-style: none;
+            float: left;
+
+            a {
+                color: white !important;
+                text-decoration: none !important;
+                font-weight: 200;
+                transition: all .3s ease-in-out;
+            }
+
+            a:hover {
+                color: $Main-Color !important;
+                transition: all .3s ease-in-out;
+            }
+        }
     }
 
-    .nav-link:hover {
-        color: $Main-Color;
-        transition: all .3s ease-in-out;
-
-    }
 
     .icons {
 
@@ -171,8 +196,8 @@ $(document).ready(function () {
 }
 
 @media only screen and (max-width:768px) {
-    .navbar-nav {
-        display: none;
+    ul {
+        display: none !important;
     }
 }
 
@@ -186,16 +211,30 @@ $(document).ready(function () {
     width: 100%;
     box-shadow: 0px 0px 5px gray;
 
-    .nav-link {
-        color: black;
-        transition: all .3s ease-in-out;
+    ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0px;
 
+        li {
+            list-style: none;
+            float: left;
+
+            a {
+                color: black !important;
+                text-decoration: none !important;
+                font-weight: 200;
+                transition: all .3s ease-in-out;
+            }
+
+            a:hover {
+                color: $Main-Color !important;
+                transition: all .3s ease-in-out;
+            }
+        }
     }
 
-    .nav-link:hover {
-        color: $Main-Color;
-        transition: all .3s ease-in-out;
-    }
 
     .line {
         background-color: $Main-Color;
