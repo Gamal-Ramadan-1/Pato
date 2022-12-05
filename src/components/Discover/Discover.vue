70f9086c915022d824d87c2c8d655e418d171a24
Gamal Ramadan <gamal.ramadan.fe@gmail.com>
2022-12-05 16:27:46 +0200

    Finished Discover Section

---
+++ src/components/Discover/Discover.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="discover">
+        <div class="text-center">
+            <p class="text-capitalize m-0">discover</p>
+            <h1 class="text-light text-uppercase fw-bold">pato place</h1>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: 'Discover'
+}
+</script>
+<style lang="scss" scoped>
+@import '../../Scss/Main.scss';
+
+.discover {
+    background-image: url('../../Imgs/bg-intro-01.jpg.webp');
+    background-size: cover;
+    clear: both;
+    background-position: top center;
+    height: 400px;
+    width: 100%;
+    background-attachment: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    p {
+        color: $Main-Color;
+        font-size: 30px;
+        font-family: $Courgette-Font;
+    }
+
+    h1 {
+        font-family: $Poppins-Font;
+        font-size: 60px;
+        letter-spacing: 10px;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
     <SliderHome />
     <Welcome />
+    <Discover />
 </template>
 <script>
+import Discover from '../components/Discover/Discover.vue';
 import SliderHome from '../components/Sliders/Slider-Home.vue';
 import Welcome from '../components/Welcome/Welcome.vue';
 
 export default {
     name: "Menu",
-    components: { SliderHome, Welcome }
+    components: { SliderHome, Welcome, Discover }
 }
 </script>
 <style lang="scss" scoped>
